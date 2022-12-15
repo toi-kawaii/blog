b87b18d083c59a973a7963df75f5da37b6d5699e
Dan <dan.makhlay@gmail.com>
2022-12-15 13:13:43 +1000

    test_commit

--- app/Http/Requests/Admin/Category/StoreRequest.php
+++ app/Http/Requests/Admin/Category/StoreRequest.php
@@ -24,7 +24,6 @@ class StoreRequest extends FormRequest
     public function rules()
     {
         return[
-            'id' => 'integer',
             'title' => 'required|string'
         ];
     }
